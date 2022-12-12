1dfa58fa3447d5a629738812dd71ea9efae5d7c3
임현홍 <bbnerino@gmail.com>
2022-12-12 15:43:18 +0900

    feat - back - login 기능 구현

--- backend/app/controllers/user.js
+++ backend/app/controllers/user.js
@@ -1,73 +1,89 @@
+const User = require("../models/user");
 
-const User = require("../models/user")
-
-
-
-exports.postSignUp = (req,res)=>{
-  if(!req.body.data){
-    res.status(400).send({message: "데이터 형식이 틀렸습니다."});
+exports.postSignUp = (req, res) => {
+  if (!req.body.data) {
+    res.status(400).send({ message: "데이터 형식이 틀렸습니다." });
   }
-  const {name,id,password} = req.body.data
-  
-  const newPost =  new User({name:name,userId:id,password:password})
-  
-  newPost.save()
-  .then(()=>{
-    res.status(201).send({message:`${id} 님 가입되었습니다`})
-  })
-  .catch(()=>{
-    res.status(400).send({message:`서버에러입니다.`})
-  })
-}
-exports.update = (req,res)=>{
-  const {name,id,password} = req.body.data
-  User.findOne({userId:req.params.userId},(err,user)=>{
-    if(err){()=>{res.status(500).send({message:"정보가 없습니다."})}}
-    else{
-      user.id = id
-      user.name = name
-      user.password = password
-      user.save((err,data)=>{
-        if(err){res.status(500).send({message:"저장 실패"})}
-        else{
-          res.send(data)
+  const { name, id, password } = req.body.data;
+
+  const newPost = new User({ name: name, userId: id, password: password });
+
+  newPost
+    .save()
+    .then(() => {
+      res.status(201).send({ message: `${id} 님 가입되었습니다` });
+    })
+    .catch(() => {
+      res.status(400).send({ message: `서버에러입니다.` });
+    });
+};
+exports.update = (req, res) => {
+  const { name, id, password } = req.body.data;
+  User.findOne({ userId: req.params.userId }, (err, user) => {
+    if (err) {
+      () => {
+        res.status(500).send({ message: "정보가 없습니다." });
+      };
+    } else {
+      user.id = id;
+      user.name = name;
+      user.password = password;
+      user.save((err, data) => {
+        if (err) {
+          res.status(500).send({ message: "저장 실패" });
+        } else {
+          res.send(data);
         }
-      })
+      });
     }
-  })
-}
+  });
+};
 
-exports.findAll = (req,res)=>{
-  User.find((err,data)=>{
-    if(err){
-      res.status(500).send({message:"에러"})
+exports.findAll = (req, res) => {
+  User.find((err, data) => {
+    if (err) {
+      res.status(500).send({ message: "에러" });
+    } else {
+      res.send(data);
     }
-    else{
-      res.send(data)
-    }
-  })
-}
+  });
+};
 
-exports.findOne = (req,res)=>{
-  User.findOne({userId:req.params.userId},(err,data)=>{
-    if(err){
-      res.status(500).send({message:"에러"})
+exports.findOne = (req, res) => {
+  User.findOne({ userId: req.params.userId }, (err, data) => {
+    if (err) {
+      res.status(500).send({ message: "에러" });
+    } else {
+      res.send(data);
     }
-    else{
-      res.send(data)
-    }
-  })
-}
+  });
+};
 
-exports.delete = async (req,res)=>{
-  User.findOne({_id:req.params.id},(err,user)=>{
-    if(err){res.status(500).send({message:"정보가 없습니다."})}
-    else{
-      User.remove({_id:req.params.id},(err,data)=>{
-        if(err){res.status(500).send({message:"에러"})}
-        else{
-          res.send({message:`${user.name} 회원 정보가 삭제되었습니다.`})
-        }})
-    }    
-  })
-}
+exports.delete = async (req, res) => {
+  User.findOne({ _id: req.params.id }, (err, user) => {
+    if (err) {
+      res.status(500).send({ message: "정보가 없습니다." });
+    } else {
+      User.remove({ _id: req.params.id }, (err, data) => {
+        if (err) {
+          res.status(500).send({ message: "에러" });
+        } else {
+          res.send({ message: `${user.name} 회원 정보가 삭제되었습니다.` });
+        }
+      });
+    }
+  });
+};
+
+exports.login = (req, res) => {
+  console.log(req.body);
+  const { id, password } = req.body.data;
+  User.findOne({ userId: id, password: password }, (err, data) => {
+    if (err) {
+      res.status(500).send({ message: "에러" });
+    } else {
+      res.send({ userGuid: data._id });
+      // console.log(JSON.stringify(data._id));
+    }
+  });
+};
--- backend/app/routes/user.js
+++ backend/app/routes/user.js
@@ -9,5 +9,5 @@ router.get("/all",userController.findAll)
 router.get("/:userId",userController.findOne)
 router.delete("/:id",userController.delete)
 router.put("/update/:id",userController.update)
-
+router.post('/login',userController.login)
 module.exports = router;
